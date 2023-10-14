9f36003bbb643afe70e4990451adc9790d2ebe4a
Felype Invictus <felype.sales@outlook.com>
2023-10-14 18:35:57 -0300

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,14 +1,6 @@
-
-
-
-
-
-    //const LINE_SPACE = '%20';
+ const country = '55';
+//const LINE_SPACE = '%20';
     const LINE_BREAK = '%0A';
-    
-      const country = '55';
-
-
-      const NOME = '*NOME:*%20';
+     const NOME = '*NOME:*%20';
       const ADRESS = '*ENDEREÇO:*%20';
       const COMPLEMENT = '*COMPLEMENTO:*%20';
