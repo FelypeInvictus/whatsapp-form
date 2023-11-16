ca8f76f83583201865fdfcb9bf9c5976cb04ca09
FelypeInvictus <felype.sales@outlook.com>
2023-11-16 17:00:28 -0300

    CORREÇÃO E MELHORIAS

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,9 @@
- const country = '55';
+
+const MESSAGE = '*INFORMAÇÃO DO CLIENTE*%20';
+const CODE_COUNTRY = '55';
 //const LINE_SPACE = '%20';
-    const LINE_BREAK = '%0A';
-     const NOME = '*NOME:*%20';
-      const ADRESS = '*ENDEREÇO:*%20';
-      const COMPLEMENT = '*COMPLEMENTO:*%20';
+const LINE_BREAK = '%0A';
+const NAME = '*NOME:*%20';
+const ADRESS = '*ENDEREÇO:*%20';
+const COMPLEMENT = '*COMPLEMENTO:*%20';
+const STATE = '*ESTADO:*%20';
