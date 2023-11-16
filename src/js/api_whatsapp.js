d7c388697fbde8ea9efc8bbdefe3f1726b011ac2
FelypeInvictus <felype.sales@outlook.com>
2023-11-16 20:43:39 -0300

    CORREÇÃO PARA VERSÃO MOBILE

--- src/js/api_whatsapp.js
+++ src/js/api_whatsapp.js
@@ -14,10 +14,11 @@ var number = window.prompt("Por favor, insira seu número:");
         alert('Número inválido!');
         return;
       }
-      // const link = `https://wa.me/${country}${number}?text=${text}`;
+      
       link = `https://web.whatsapp.com/send?phone=${CODE_COUNTRY}${number}&text=${MESSAGE+LINE_BREAK+NAME+name+LINE_BREAK+ADRESS+endereco+LINE_BREAK+COMPLEMENT+complemento+LINE_BREAK+STATE+estado}`;
       if ((new MobileDetect(window.navigator.userAgent)).mobile()) {
-        link = `https://api.whatsapp.com/send?phone=${CODE_COUNTRY}${number}&text=${text}`;
+        console.log("chegou aqui");
+        link = `https://api.whatsapp.com/send?phone=${CODE_COUNTRY}${number}&text=${MESSAGE+LINE_BREAK+NAME+name+LINE_BREAK+ADRESS+endereco+LINE_BREAK+COMPLEMENT+complemento+LINE_BREAK+STATE+estado}`;
       }
       console.log(number, link);
       window.open(link);
